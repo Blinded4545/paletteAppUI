52aea42cb4883cb78c98a428a3719dbd1ff8e47b
Blinded <bluemistery82@gmail.com>
2023-09-27 21:53:25 -0500

    Created login vue and implemented it, deletion of base files

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
 </template>
 
 <style>
+
+html{
+  margin: 0;
+  width: 100vw;
+  height: 100vh;
+}
+
+body{
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/views/login.vue
@@ -0,0 +1,112 @@
+<template>
+  <div id="login">
+    <div id="wrapper">
+      <div id="logintxt">
+        <h1>Log In</h1>
+      </div>
+      <div>
+        <input class="form" placeholder="User" v-model="usr"/>
+      </div>
+      <div>
+        <input class="form" placeholder="Password" v-model="pwd"/>
+      </div>
+      <div>
+        <button id="logInbutton" @click="login">Log In</button>
+      </div>
+      <div>
+        <button id="register" @click="register">Register</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default{
+  name: "loginView",
+  data(){
+    return {
+      usr: "",
+      pwd: "",
+      badLogin: false
+    }
+  },
+  methods: {
+    login(){
+      console.log(this.usr, this.pwd);
+    },
+    register(){
+
+    }
+  }
+}
+
+</script>
+
+<style>
+
+#login{
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  display: grid;
+  align-content: center;
+  justify-content: center;
+  background: linear-gradient(260deg, #2874A6, #148F77);
+}
+
+#wrapper{
+  border: 1px black solid;
+  border-radius: 10px;
+  width:fit-content;
+  background-color: rgba(26, 188, 156, .6);
+}
+
+#logintxt{
+  margin: 10px;
+}
+
+.form{
+  padding: 5px;
+  margin: 5px 20px 10px 20px;
+  border-radius: 10px;
+  border: none;
+  transition: all .3s ease;
+}
+.form:hover{
+  transform: scale(1.1);
+}
+.form:focus{
+  transform: scale(1.1);
+  background-color: #58D68D;
+  outline: none;
+}
+.form::placeholder{
+  padding-left: 5%;
+  transform: scale(1.1);
+  border: none;
+}
+
+#logInbutton{
+  
+  font-size: 1.25rem;
+}
+#logInbutton, #register{
+  padding: 5px 15px;
+  border: none;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  transition: all 0.3s linear;
+}
+#register{
+  margin-bottom: 15px;
+}
+
+#logInbutton:hover{
+  transform: scale(1.3);
+}
+#register:hover{
+  transform: scale(1.3);
+}
+
+</style>
